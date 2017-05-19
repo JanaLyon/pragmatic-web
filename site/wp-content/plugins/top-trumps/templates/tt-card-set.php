71a9d5b9b2a9bd06269d45268d8a57dc88c98224
jana.lyon <jana.lyon@icrossing.co.uk>
2017-05-19 11:59:16 +0100

    Menu structure and styling added

--- site/wp-content/plugins/top-trumps/templates/tt-card-set.php
+++ site/wp-content/plugins/top-trumps/templates/tt-card-set.php
@@ -20,55 +20,66 @@ while ($topTrumpSet_query->have_posts()) {
 
     //var_dump($height_value)
     ?>
+    <!--div class="tt_card-element"-->
+        <div class="tt_card-container ontouchstart=" this.classList.toggle(
+        'hover');">
+        <div class="tt_card">
+            <div class="front">
+                <div class="name-container">
+                    <?php echo the_title(); ?>
+                </div>
+                <div class="image-container">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <div class="data-container">
+                    <div class="tt-attribute">
+                        <p>Height (cm)</p>
+                        <p>Intelligence</p>
+                        <p>Strength</p>
+                        <p>Speed</p>
+                        <p>Agility</p>
+                        <p>Fighting skills</p>
+                    </div>
+                    <div class="tt-data">
+                        <p><?php echo $height_value; ?></p>
+                        <p><?php echo $intelligence_value ?></p>
+                        <p><?php echo $strength_value ?></p>
+                        <p><?php echo $speed_value ?></p>
+                        <p><?php echo $agility_value ?></p>
+                        <p><?php echo $fighting_skills_value ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <div class="description-container">
+                    <div class="name-container">Origin story</div>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and
+                        Consonantia, there
+                        live the blind texts. Separated they live in Bookmarksgrove right at the coast of
+                        the semantics, a large language ocean. It is a paradisematic country, in which
+                        roasted parts of sentences fly into your mouth. </p>
+                </div>
+                <div class="image-container">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <div class="name-container">
+                    <?php echo the_title(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--div class="tt_selections">
+        <div class="tt_compare">
+            <i class="fa fa-check-square-o" aria-hidden="true"></i>
+        </div>
+        <div class="tt_favourite">
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+        </div>
+    </div>
+    </div-->
 
-    <div class="tt_card-container ontouchstart=" this.classList.toggle(
-    'hover');">
-    <div class="tt_card">
-        <div class="front">
-            <div class="name-container">
-                <?php echo the_title(); ?>
-            </div>
-            <div class="image-container">
-                <?php the_post_thumbnail(); ?>
-            </div>
-            <div class="data-container">
-                <div class="tt-attribute">
-                    <p>Height (cm)</p>
-                    <p>Intelligence</p>
-                    <p>Strength</p>
-                    <p>Speed</p>
-                    <p>Agility</p>
-                    <p>Fighting skills</p>
-                </div>
-                <div class="tt-data">
-                    <p><?php echo $height_value; ?></p>
-                    <p><?php echo $intelligence_value ?></p>
-                    <p><?php echo $strength_value ?></p>
-                    <p><?php echo $speed_value ?></p>
-                    <p><?php echo $agility_value ?></p>
-                    <p><?php echo $fighting_skills_value ?></p>
-                </div>
-            </div>
-        </div>
-        <div class="back">
-            <div class="description-container">
-                <div class="name-container">Origin story</div>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and
-                    Consonantia, there
-                    live the blind texts. Separated they live in Bookmarksgrove right at the coast of
-                    the semantics, a large language ocean. It is a paradisematic country, in which
-                    roasted parts of sentences fly into your mouth. </p>
-            </div>
-            <div class="image-container">
-                <?php the_post_thumbnail(); ?>
-            </div>
-            <div class="name-container">
-                <?php echo the_title(); ?>
-            </div>
-        </div>
-    </div>
-    </div>
     <?php
 } //if ($this_query)
 wp_reset_query(); // Restore global post data stored by the_post().
 ?>
+
--- site/wp-content/plugins/top-trumps/templates/tt-set.php
+++ site/wp-content/plugins/top-trumps/templates/tt-set.php
@@ -9,6 +9,7 @@ get_header();
 /*
  * TODO
  * Import styles and scripts with enqueue
+ * Redo styling to work without bootstrap
  */
 ?>
     <link rel="stylesheet" href="<?php echo plugins_url('top-trumps/css/main.css') ?>">
@@ -16,11 +17,25 @@ get_header();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <script src="<?php echo plugins_url('top-trumps/js/main.js') ?>"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="<?php echo plugins_url('top-trumps/js/main.js') ?>"></script>
 
+    <div class="tt_menu">
+        <div class="wrap">
+        <div class="tt_button-container">
+            <div class="tt_menu-button">View favourites <i class="fa fa-heart" aria-hidden="true"></i></div>
+            <div class="tt_menu-label">Sort by <i class="fa fa-caret-right" aria-hidden="true"></i></div>
+            <div class="tt_menu-button">Name</div>
+            <div class="tt_menu-button">Height</div>
+            <div class="tt_menu-button">Intelligence</div>
+            <div class="tt_menu-button">Strength</div>
+            <div class="tt_menu-button">Agility</div>
+            <div class="tt_menu-button">Fighting skills</div>
+        </div>
+        </div>
+    </div>
     <div class="wrap">
-        <?php tt_get_template( "tt-card-set.php" ); ?>
+        <?php tt_get_template("tt-card-set.php"); ?>
     </div>
 <?php
 get_footer();
